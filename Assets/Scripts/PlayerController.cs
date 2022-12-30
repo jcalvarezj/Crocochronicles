7090c58af48b6a52f9a08bf8de6b742b2df179e1
jcalvarezj <jcalvarezj@gmail.com>
2022-12-30 15:29:18 -0500

    Add raycasting jump logic and project SLN file

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
+    public float jumpThreshold = 1.1f;
+    public float jumpForce = 6f;
+    public LayerMask groundMask;
+    Rigidbody2D playerRigidbody;
+
+    // Awake is called before start to prepare the game object
+    void Awake() {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) {
+            Jump();
+        }
+    }
+
+    // Jump makes the player object jump
+    void Jump() {
+        if (IsTouchingGround()) {
+            playerRigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    // IsTouchingGround indicates whether the player object is touching the ground (near enough)
+    bool IsTouchingGround() {
+        return Physics2D.Raycast(this.transform.position, Vector2.down, jumpThreshold, groundMask);
     }
 }
