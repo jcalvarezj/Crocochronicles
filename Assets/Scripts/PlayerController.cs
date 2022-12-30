d3a92ef7575e26d0df24c764932b020c17a333fe
jcalvarezj <jcalvarezj@gmail.com>
2022-12-30 18:26:22 -0500

    Implement jump animation transition

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,19 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+    const string STATE_ALIVE = "isAlive";
+    const string STATE_ON_GROUND = "isOnGround";
+
     public float jumpThreshold = 1.1f;
     public float jumpForce = 6f;
     public LayerMask groundMask;
     Rigidbody2D playerRigidbody;
+    Animator animator;
 
     // Awake is called before start to prepare the game object
     void Awake() {
         playerRigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Start is called before the first frame update
     void Start() {
-        
+        animator.SetBool(STATE_ALIVE, true);
+        animator.SetBool(STATE_ON_GROUND, true);
     }
 
     // Update is called once per frame
@@ -23,6 +29,8 @@ public class PlayerController : MonoBehaviour {
         if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) {
             Jump();
         }
+
+        animator.SetBool(STATE_ON_GROUND, IsTouchingGround());
     }
 
     // Jump makes the player object jump
